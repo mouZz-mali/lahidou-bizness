54e0e69c459a27a09c287f69abbf163ef99895fb
mouZz-mali <moussacoulibaly06092004@gmail.com>
2025-06-07 20:14:59 +0000

    Description de ta modification

--- lahidou.js
+++ lahidou.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll(".ajouter-panier");
-    alert("La  fête approche ! Vous avez frappé à la bonne porte !"); 
+    // alert("La  fête approche ! Vous avez frappé à la bonne porte !"); 
     
     // Effet d'animation sur les produits
     const produits = document.querySelectorAll(".produit");
@@ -14,4 +14,12 @@ document.addEventListener("DOMContentLoaded", function() {
             produit.style.transform = "scale(1)";
         });
     });
+    
+    setTimeout(() => {
+        const tongue = document.getElementById('chameleon-tongue');
+        tongue.style.width = "160px"; // La langue sort
+        setTimeout(() => {
+            tongue.style.width = "0"; // La langue rentre
+        }, 1200);
+    }, 800); // Délai après chargement
 });
